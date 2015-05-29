e4d8c50d74c78f50be7b54e06d9930d3f29b19ad
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-05-29 13:50:53 +0100

    resource compile

--- cljs-out/none/cljs/clustermap/formats/html.cljs
+++
@@ -1,9 +0,0 @@
-(ns clustermap.formats.html
-  (:require [clojure.string :as str]))
-
-(defn- combine-classes
-  [& classes]
-  (->> classes
-       (filter identity)
-       (filter (complement str/blank?))
-       (str/join " ")))
